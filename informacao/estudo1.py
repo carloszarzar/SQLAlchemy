ede3d2ff770b1cf9380be2e6db9600faf6393fec
Carlos Zarzar <carlos_zarzar@outlook.com>
2025-08-04 10:42:19 -0300

    docs: Organizando os documentos e script de estudo em python na pasta informacao

---
+++ informacao/estudo1.py
@@ -0,0 +1,11 @@
+# Classe das entidades dos repositórios
+class OlaMundo:
+    def __enter__(self):
+        print('Estou entrando!')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Estou saindo!')
+
+
+with OlaMundo() as ola:
+    print('Estou no meio!!!')
